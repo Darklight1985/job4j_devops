260254793cba26f1184d29c2978db4d063486401
Darklight1985 <darklight1985@mail.ru>
2025-04-04 22:10:14 +0300

    Вернул тест

--- src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
+++ src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
@@ -71,4 +71,13 @@ class CalcControllerTest {
         assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
         assertThat(output.getBody()).isEqualTo(expected);
     }
+
+    @Test
+    public void whenTwoTimesThreeThenSix() {
+        var input = new TwoArgs(3, 3);
+        var expected = new Result(9);
+        var output = new CalcController().times(input);
+        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(output.getBody()).isEqualTo(expected);
+    }
 }
