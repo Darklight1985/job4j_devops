24e9cb157bfa45f8c6975eea053d1178a9473bab
Darklight1985 <darklight1985@mail.ru>
2025-04-22 21:50:40 +0300

    Добавил настройку для провреки сборки кэша

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,17 @@
 rootProject.name = "DevOps"
 
-//buildCache {
-// remote<HttpBuildCache> {
-   //     url = uri(System.getenv("GRADLE_REMOTE_CACHE_URL"))
-     //   isAllowInsecureProtocol = true
-       // isAllowUntrustedServer = true
-        //isPush = System.getenv("GRADLE_REMOTE_CACHE_PUSH").toBoolean()
-        //credentials {
-         //   username = System.getenv("GRADLE_REMOTE_CACHE_USERNAME")
-          //  password = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD")
-       // }
-   // }
-// }
+buildCache {
+    val urlCache = System.getenv("GRADLE_REMOTE_CACHE_URL")
+    if (urlCache != null) {
+        remote<HttpBuildCache> {
+            url = uri(System.getenv("GRADLE_REMOTE_CACHE_URL"))
+            isAllowInsecureProtocol = true
+            isAllowUntrustedServer = true
+            isPush = System.getenv("GRADLE_REMOTE_CACHE_PUSH").toBoolean()
+            credentials {
+                username = System.getenv("GRADLE_REMOTE_CACHE_USERNAME")
+                password = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD")
+            }
+        }
+    }
+ }
