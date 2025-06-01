55f4d8544dd40b304cd77dab799317af970eb260
Darklight1985 <darklight1985@mail.ru>
2025-06-01 20:40:56 +0300

    Отключил liquibase при тестах

--- src/test/java/ru/job4j/devops/CalcApplicationTests.java
+++ src/test/java/ru/job4j/devops/CalcApplicationTests.java
@@ -1,17 +1,26 @@
 package ru.job4j.devops;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        LiquibaseAutoConfiguration.class
+})
 class CalcApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
-    @Test
-    void mainMethodTest() {
-        CalcApplication.main(new String[] {});
-    }
+//    @Test
+//    void mainMethodTest() {
+//        CalcApplication.main(new String[] {});
+//    }
 }
